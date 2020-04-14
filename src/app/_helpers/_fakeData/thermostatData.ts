88714a4e75d0cbc4f57a8334c9bbce087f53bca6
mughees <mugheesilyas@live.com>
2020-04-14 06:15:16 +0200

    cleaning data

--- src/app/_helpers/_fakeData/thermostatData.ts
+++ src/app/_helpers/_fakeData/thermostatData.ts
@@ -61,32 +61,32 @@ let data = [
             id: 25,
             household_token:'Bearer fake-jwt-token3',
             location:'rio',
-            thermostat_id:14,
+            thermostat_id:15,
             temperature:22.5,
             humidity:33,
             battery_charge:60,
         }
     },{
         id:16,
-        household_token:'Bearer fake-jwt-token3',
-        location:'rio',
+        household_token:'Bearer fake-jwt-token4',
+        location:'islamabad',
         latestReading:{
             id: 26,
-            household_token:'Bearer fake-jwt-token3',
-            location:'rio',
-            thermostat_id:14,
+            household_token:'Bearer fake-jwt-token4',
+            location:'islamabad',
+            thermostat_id:16,
             temperature:22.5,
             humidity:33,
             battery_charge:90,
         }
     },{
         id:17,
-        household_token:'Bearer fake-jwt-token3',
-        location:'rio',
+        household_token:'Bearer fake-jwt-token4',
+        location:'delhi',
         latestReading:{
             id: 27,
-            household_token:'Bearer fake-jwt-token3',
-            location:'rio',
+            household_token:'Bearer fake-jwt-token4',
+            location:'delhi',
             thermostat_id:14,
             temperature:22.5,
             humidity:33,
@@ -94,12 +94,12 @@ let data = [
         }
     },{
         id:18,
-        household_token:'Bearer fake-jwt-token3',
-        location:'rio',
+        household_token:'Bearer fake-jwt-token4',
+        location:'ohio',
         latestReading:{
             id: 28,
-            household_token:'Bearer fake-jwt-token3',
-            location:'rio',
+            household_token:'Bearer fake-jwt-token4',
+            location:'ohio',
             thermostat_id:14,
             temperature:22.5,
             humidity:33,
@@ -107,12 +107,12 @@ let data = [
         }
     },{
         id:19,
-        household_token:'Bearer fake-jwt-token3',
-        location:'rio',
+        household_token:'Bearer fake-jwt-token5',
+        location:'barcelona',
         latestReading:{
             id: 29,
-            household_token:'Bearer fake-jwt-token3',
-            location:'rio',
+            household_token:'Bearer fake-jwt-token5',
+            location:'barcelona',
             thermostat_id:15,
             temperature:22.5,
             humidity:33,
@@ -120,12 +120,12 @@ let data = [
         }
     },{
         id:51,
-        household_token:'Bearer fake-jwt-token3',
-        location:'rio',
+        household_token:'Bearer fake-jwt-token5',
+        location:'madrid',
         latestReading:{
             id: 61,
-            household_token:'Bearer fake-jwt-token3',
-            location:'rio',
+            household_token:'Bearer fake-jwt-token5',
+            location:'madrid',
             thermostat_id:15,
             temperature:22.5,
             humidity:33,
@@ -133,12 +133,12 @@ let data = [
         }
     },{
         id:52,
-        household_token:'Bearer fake-jwt-token3',
-        location:'rio',
+        household_token:'Bearer fake-jwt-token5',
+        location:'new york',
         latestReading:{
             id: 62,
-            household_token:'Bearer fake-jwt-token3',
-            location:'rio',
+            household_token:'Bearer fake-jwt-token5',
+            location:'new york',
             thermostat_id:15,
             temperature:22.5,
             humidity:33,
